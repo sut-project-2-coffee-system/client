7103dfff3ede336677fff8e3b6a183ebce2f222c
Miwtoo <miwyugioh@gmail.com>
2019-08-01 21:45:45 +0700

    remove unuse

--- src/components/Bigbutton.jsx
+++ src/components/Bigbutton.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import Paper from '@material-ui/core/Paper';
-import Card from '@material-ui/core/Card';
 import './mycss.css';
 import Typography from '@material-ui/core/Typography';
 

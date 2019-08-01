b47b50f25663950c1d40e632fdab7104db0c87f2
Miwtoo <miwyugioh@gmail.com>
2019-08-01 21:37:01 +0700

    close#4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/18527723_1300430956678248_5746867614533908651_n.jpg?_nc_cat=103&_nc_oc=AQkFxAUPUMlSCUVaGx0XmbMFel6rhLr1lxk5fsE8QGQzjPaa9_0pMs6rrgZ9IlfvTgXAAFr0M5l07WqmwwoKX2JN&_nc_ht=scontent.fbkk5-4.fna&oh=59110ed9e9c5fc8ee5bfde887aadfece&oe=5DE87D6C" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }

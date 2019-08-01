8cf1cf63b5aa27e169dcd56fb6da87b0410a5c89
anantakit <frame.zz@hotmail.com>
2019-08-02 01:07:04 +0700

    close #5 do router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-
-
-
+import { Route,Switch } from 'react-router-dom'
+import Member from './pages/Member';
 
 function App() {
   
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/member" component={Member}></Route>
+      </Switch>
     </div>
+    
   );
 }
 

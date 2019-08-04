399386ac4ba1e54a4dc24a357361ada3ed32decf
anantakit <frame.zz@hotmail.com>
2019-08-04 17:58:53 +0700

    close #6 reactstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
-import { Route,Switch } from 'react-router-dom'
-import Member from './pages/Member';
 
 function App() {
-  
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/home" component={Home}></Route>
-        <Route path="/member" component={Member}></Route>
-      </Switch>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    
   );
 }
 
